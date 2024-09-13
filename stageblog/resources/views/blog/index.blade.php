a0857f520624fd103b66f1061ac55c559477a0f5
Vinnie <34916@ma-web.nl>
2024-09-13 13:03:06 +0200

    creating model

---
+++ stageblog/resources/views/blog/index.blade.php
@@ -0,0 +1,1 @@
+<x-app-layout>h</x-app-layout>
--- stageblog/routes/web.php
+++ stageblog/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +16,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/about', [ \App\Http\Controllers\AboutController::class, 'about'])->name('about');
+    Route::get('/blog', [ \App\Http\Controllers\BlogController::class, 'index'])->name('blog.index');
+    Route::get('/contact', [ \App\Http\Controllers\ContactController::class, 'contact'])->name('contact.form');
 });
 
 require __DIR__.'/auth.php';
